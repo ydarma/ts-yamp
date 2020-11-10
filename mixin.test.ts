e0fc018246467a3e80c57e2399991332e3bc85e4
YDarma <yves.darmaillac@arone.com>
2020-11-10 16:37:44 +0100

    Parameter builder

--- mixin.test.ts
+++ mixin.test.ts
@@ -1,5 +1,5 @@
 import test from "tape";
-import { mixin, constr } from "./mixin";
+import { mixin, constr, params } from "./mixin";
 import { Bird, Informer, Man, Singer } from "./test-types";
 
 test("Mixin class", (t) => {
@@ -34,13 +34,13 @@ test("Mixin class with override", (t) => {
 
 test("Mixin class and instance", (t) => {
   class TestMixin {}
-  const BirdNamedTiti = mixin(TestMixin, Bird, constr(TestMixin, Bird, "Titi"));
+  const BirdNamedTiti = mixin(TestMixin, Bird, constr(TestMixin, Bird));
   const BirdWhichSing = mixin(
     BirdNamedTiti,
     Singer,
     constr(BirdNamedTiti, Singer)
   );
-  const myBird = new BirdWhichSing();
+  const myBird = new BirdWhichSing([[], "Titi"]);
   t.equal(myBird.name, "Titi");
   t.equal(myBird.sing(), "I sing like a bird.");
   t.equal(myBird.when, "In the morning.");
@@ -49,7 +49,7 @@ test("Mixin class and instance", (t) => {
 
 test("Mixin class and instance with constructor", (t) => {
   class TestMixin {}
-  const BirdNamedTiti = mixin(TestMixin, Bird, constr(TestMixin, Bird, "Titi"));
+  const BirdNamedTiti = mixin(TestMixin, Bird, constr(TestMixin, Bird));
   const birdConstructor = function (this: Bird & Singer, when: string) {
     this.when = when;
   };
@@ -58,27 +58,43 @@ test("Mixin class and instance with constructor", (t) => {
     Singer,
     constr(birdConstructor, constr(BirdNamedTiti, Singer))
   );
-  const myBird = new BirdWhichSing("During the night.");
+  const myBird = new BirdWhichSing(["During the night."], [[], "Titi"]);
   t.equal(myBird.name, "Titi");
   t.equal(myBird.sing(), "I sing like a bird.");
   t.equal(myBird.when, "During the night.");
   t.end();
 });
 
+test("Parameter builder", (t) => {
+  const p1 = params();
+  t.deepEqual(p1.args, []);
+  const p2 = p1.params("Hello", 1);
+  t.deepEqual(p2.args, [[], "Hello", 1]);
+  const p = params().params("Joe").params().params("Every day.");
+  t.deepEqual(p.args, [[[[], "Joe"]], "Every day."]);
+  t.end();
+});
+
+class TestMixin {}
+const ManNamedJoe = mixin(TestMixin, Man, constr(TestMixin, Man));
+const ManWhoInform = mixin(
+  ManNamedJoe,
+  Informer,
+  constr(ManNamedJoe, Informer)
+);
+const ManWhoSing = mixin(ManWhoInform, Singer, constr(ManWhoInform, Singer));
+
 test("Mixin class and instance with override", (t) => {
-  class TestMixin {}
-  const ManNamedJoe = mixin(TestMixin, Man, constr(TestMixin, Man, "Joe"));
-  const ManWhoInform = mixin(
-    ManNamedJoe,
-    Informer,
-    constr(ManNamedJoe, Informer)
-  );
-  const ManWhoSing = mixin(
-    ManWhoInform,
-    Singer,
-    constr(ManWhoInform, Singer, "Every day.")
-  );
-  const joe = new ManWhoSing();
+  const joe = new ManWhoSing([[[], "Joe"]], "Every day.");
+  t.equal(joe.name, "Joe");
+  t.equal(joe.sing(), "I say every thing.");
+  t.equal(joe.when, "Every day.");
+  t.end();
+});
+
+test("Mixin class and instance with parameter builder", (t) => {
+  const p = params().params("Joe").params().params("Every day.");
+  const joe = new ManWhoSing(...p.args);
   t.equal(joe.name, "Joe");
   t.equal(joe.sing(), "I say every thing.");
   t.equal(joe.when, "Every day.");
--- mixin.ts
+++ mixin.ts
@@ -23,13 +23,29 @@ export function mixin<T, S extends unknown[], U, R extends unknown[] = S>(
 
 export function constr<T, S extends unknown[], U, R extends unknown[]>(
   t: Ctor<T, S> | Ftor<T, S>,
-  u: Ctor<U, R> | Ftor<U, R>,
-  ...params: R
-): Ctor<T & U, S> {
+  u: Ctor<U, R> | Ftor<U, R>
+): Ctor<T & U, [S, ...R]> {
   const tCtor = t as Ctor<T, S>;
   const uCtor = u as Ctor<U, R>;
-  const fn = function (this: T & U, ...args: S) {
-    Object.assign(this, new uCtor(...params), new tCtor(...args));
+  const fn = function (this: T & U, args0: S, ...args1: R) {
+    Object.assign(this, new uCtor(...args1), new tCtor(...args0));
   };
-  return (fn as unknown) as Ctor<T & U, S>;
+  return (fn as unknown) as Ctor<T & U, [S, ...R]>;
+}
+
+interface IParams<T extends unknown[]> {
+  readonly args: T;
+  params<U extends unknown[]>(...args: U): IParams<[T, ...U]>;
+}
+
+class Params<T extends unknown[]> implements IParams<T> {
+  constructor(readonly args: T = <never>[]) {}
+
+  params<U extends unknown[]>(...args: U): IParams<[T, ...U]> {
+    return new Params([this.args, ...args]);
+  }
+}
+
+export function params<T extends unknown[]>(...args: T): IParams<T> {
+  return new Params<T>(args);
 }
