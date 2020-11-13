78025d1e1431a611818a412371780a22b05c2ac4
YDarma <yves.darmaillac@arone.com>
2020-11-13 17:53:04 +0100

    rollup to main directory

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from "@wessberg/rollup-plugin-ts";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import generatePackageJson from "rollup-plugin-generate-package-json";
 
 export default {
   input: "./src/mixin.ts",
@@ -9,16 +8,5 @@ export default {
     file: "./index.js",
     format: "cjs",
   },
-  plugins: [
-    typescript(),
-    resolve(),
-    commonjs(),
-    generatePackageJson({
-      outputFolder: "dist",
-      baseContents: (pkg) => ({
-        name: pkg.name,
-        version: pkg.version,
-      }),
-    }),
-  ],
+  plugins: [typescript(), resolve(), commonjs()],
 };
